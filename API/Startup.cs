19343bff96063d9fb0b604211e4ef6fb71c864a8
Ervin Hennrich <ervinhennrich@gmail.com>
2021-11-13 18:38:24 -0600

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -18,8 +18,7 @@ namespace API
 {
     public class Startup
     {
-
-        private readonly IConfiguration _config;
+        private IConfiguration _config;
         public Startup(IConfiguration config)
         {
             _config = config;
@@ -38,6 +37,13 @@ namespace API
             {
                 opt.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>{
+                    policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,9 +56,9 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
-            //app.UseHttpsRedirection();
-
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
